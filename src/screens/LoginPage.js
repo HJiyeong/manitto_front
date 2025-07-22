b91928d40ea9e35789a2ab06e85b957869e2f35f
BaeYoona <jinalks43@gmail.com>
2025-07-22 20:39:40 +0900

    [Add] 카카오 로그인 버튼 이미지 설정

--- src/screens/LoginPage.js
+++ src/screens/LoginPage.js
@@ -1,27 +1,30 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import Layout from '../components/Layout';
 import startBg from '../assets/images/start_background.png';
 import { kakaoLogin } from '../hooks/useAuth';
+import kakaoLoginBtn from '../assets/images/buttons/kakao_login_large_wide.png';
 
 const LoginPage = () => {
   return (
     <Layout innerBackground={startBg}>
-      <div style={{
+      <div
+        style={{
           textAlign: 'center',
           display: 'flex',
-          flexDirection : 'column',
-          justifyContent : 'center',
-          alignItems : 'center',
-          height : '100vh',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center',
+          height: '100vh',
           textAlign: 'center',
-          
-        }}>
+        }}
+      >
         <h1>John's</h1>
         <h1>Manitto</h1>
-        <button onClick={kakaoLogin}>카카오 로그인</button>
-
-
+        <img
+          src={kakaoLoginBtn}
+          onClick={kakaoLogin}
+          style={{ width: '400px', cursor: 'pointer' }}
+        />
       </div>
     </Layout>
   );
